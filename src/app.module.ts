c7a3125a9cf09a542af7581b0797e0b8d1e1f157
Josinee <ja.besseling@student.avans.nl>
2024-12-20 10:43:33 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -41,6 +41,7 @@ export class AppModule {
       type: "postgres",
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
+      password: process.env.DB_PASSWORD,
       username: process.env.DB_USER,
       database: process.env.DB_DATABASE,
     });
