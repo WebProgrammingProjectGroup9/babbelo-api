52eea979c74d9aa8deda06c01f06b94e26a1f881
Josinee <ja.besseling@student.avans.nl>
2025-01-06 11:26:02 +0100

    changed response

--- src/modules/account/account.service.ts
+++ src/modules/account/account.service.ts
@@ -2,19 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { Account } from './entities/account.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { AccountDto } from './account.dto';
 
 @Injectable()
 export class AccountService {
 
     constructor( @InjectRepository(Account) private readonly accountRepo: Repository<Account>){}
 
-    async getAccount(id: number): Promise<Account> {
-        return this.accountRepo.findOne({ where: { id } });
-    }
+    async getAccount(id: number): Promise<AccountDto> {
+        const account = await this.accountRepo.findOne({ where: { id } });
+        return { name: account.name, emailAddress: account.emailAddress };
+      }
 
-    async getAll(): Promise<Account[]> {
-        return this.accountRepo.find();
-    }
+      async getAll(): Promise<AccountDto[]> {
+        const accounts = await this.accountRepo.find();
+
+        return accounts.map((accounts) => ({ name: accounts.name, emailAddress: accounts.emailAddress }));
+      }
 
     
 }
