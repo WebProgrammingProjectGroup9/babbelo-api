d429346bf45bead15353a79c7b81ca1b623fc3cc
Josinee <ja.besseling@student.avans.nl>
2025-01-06 10:15:28 +0100

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
@@ -16,6 +17,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const corsOptions: CorsOptions = {};
   app.enableCors(corsOptions);
+  const config = new DocumentBuilder()
+    .setTitle('Babbelo')
+    .setDescription('Babbelo-api')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   //app.useGlobalInterceptors(new ApiResponseInterceptor());
   const port = parseInt(process.env.PORT, 10) || 3100;
