8b4247264f1aa6ba73da347fa522473bd5e92cc4
Josinee <ja.besseling@student.avans.nl>
2025-01-20 13:29:50 +0100

    Update util.ts

--- src/util/util.ts
+++ src/util/util.ts
@@ -6,35 +6,21 @@ export class util {
   }
 
   static transformPhotos(entity: any): any {
-    console.log(entity);
     if (!entity) return entity;
   
-    if (entity.photo) {
-      console.log('Photo exists:', entity.photo);
-      console.log('Is Buffer:', Buffer.isBuffer(entity.photo));
-    }
-    if (entity.profileImgUrl) {
-      console.log('ProfileImgUrl exists:', entity.profileImgUrl);
-      console.log('Is Buffer:', Buffer.isBuffer(entity.profileImgUrl));
-    }
   
     if (entity.photo && Buffer.isBuffer(entity.photo)) {
-      console.log('Transforming photo');
       entity.photo = util.getPhoto(entity.photo);
     }
     if (entity.profileImgUrl && Buffer.isBuffer(entity.profileImgUrl)) {
-      console.log('Transforming profileImgUrl');
       entity.profileImgUrl = util.getPhoto(entity.profileImgUrl);
     }
   
     for (const key in entity) {
-      console.log('key', key);
       if (Array.isArray(entity[key])) {
-        console.log('array ', key);
-        entity[key] = entity[key].map((item) => util.transformPhotos(item)); // Preserve context here
+        entity[key] = entity[key].map((item) => util.transformPhotos(item));
       } else if (typeof entity[key] === 'object' && entity[key] !== null) {
-        console.log('nested', key);
-        entity[key] = util.transformPhotos(entity[key]); // Preserve context here
+        entity[key] = util.transformPhotos(entity[key]);
       }
     }
   
